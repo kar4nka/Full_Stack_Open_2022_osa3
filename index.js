ae5db68ec1f81a206d0ecc35ed63470762561ac3
karanka <kar4nka@protonmail.com>
2023-04-22 18:57:46 +0300

    3.5 done

--- index.js
+++ index.js
@@ -55,6 +55,28 @@ app.delete('/api/persons/:id', (request,response) => {
   response.status(204).end()
 })
 
+
+app.use(express.json())
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random()*10000)
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
