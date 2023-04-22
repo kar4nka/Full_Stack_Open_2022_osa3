53329b406feae04eba064ff61321a825b14cb89f
karanka <kar4nka@protonmail.com>
2023-04-22 18:23:43 +0300

    3.4 done

--- index.js
+++ index.js
@@ -29,17 +29,30 @@ app.get('/api/persons/', (request,response) => {
 })
 
 app.get('/api/persons/:id', (request,response) => {
-  let id = Number(request.params.id)
+  const id = Number(request.params.id)
   console.log(id)
-  response.json(persons.find(person => id === person.id))
+  const person = persons.find(person => id === person.id)
+
+  if(person){
+    response.json(person)
+  }else{
+    response.status(404).end()
+  }
 })
 
 
 app.get('/info', (request,response) => {
-    response.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${Date()}</p>
-    `)
+  response.send(`
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${Date()}</p>
+  `)
+})
+
+
+app.delete('/api/persons/:id', (request,response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
 })
 
 const PORT = 3001
