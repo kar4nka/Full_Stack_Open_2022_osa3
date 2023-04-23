163e4c203dfca187d1580e4769ac7aa1f22a9734
karanka <kar4nka@protonmail.com>
2023-04-23 13:55:34 +0300

    3.8 done

--- index.js
+++ index.js
@@ -25,8 +25,9 @@ let persons = [
   }
 ]
 
-app.use(morgan('tiny'))
 app.use(express.json())
+morgan.token('postobject', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postobject'))
 
 app.get('/api/persons/', (request,response) => {
   response.json(persons)
@@ -79,7 +80,6 @@ app.post('/api/persons', (request, response) => {
   }
 
   persons = persons.concat(person)
-
   response.json(person)
 })
 
