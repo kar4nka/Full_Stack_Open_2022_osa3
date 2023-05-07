aa3f6d333d4a1ff9a2e383796949892e887c9a7b
karanka <kar4nka@protonmail.com>
2023-05-07 19:29:44 +0300

    3.17 done

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ app.get('/api/persons/', (request,response) => {
   .catch(error => next(error))
 })
 
-
 app.get('/api/persons/:id', (request,response, next) => {
-
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
@@ -71,6 +69,22 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+
 /* Error handling */
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
@@ -81,10 +95,7 @@ const errorHandler = (error, request, response, next) => {
 
   next(error)
 }
-
 app.use(errorHandler)
-
-
 
 
 
